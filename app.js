e75dbbd283548e347a92351ac2a2d41d8ea5541c
sanjibdey104 <sanjibdey.dey4@gmail.com>
2021-06-19 23:11:29 +0530

    fix: scroll tracker responsiveness with width resize

--- app.js
+++ app.js
@@ -2,20 +2,33 @@ const scrollTracker = document.querySelector('.scroll-tracker');
 
 let body = document.body;
 let html = document.documentElement;
-let totalDocHeight = Math.max(
-  body.scrollHeight, html.scrollHeight,
-  body.offsetHeight, html.offsetHeight,
-  body.clientHeight, html.clientHeight
-);
-const clientHeight = html.clientHeight;
-let heightToScroll = totalDocHeight - clientHeight;
 
-window.addEventListener('scroll', () => {
-  let scrolledFromTop = html.scrollTop;
-  let yetToScroll = heightToScroll - scrolledFromTop;
-  
-  let rawScrolledPercentage = 100 - ((yetToScroll/heightToScroll)*100);
-  let ceiledScrollPercentage = Math.round(rawScrolledPercentage);
+window.addEventListener('DOMContentLoaded', () => {
+    adjustScrollTracker();
+})
 
-  scrollTracker.style.width=`${ceiledScrollPercentage}%`
-});
+
+window.addEventListener('resize', () => {
+   adjustScrollTracker();
+})
+
+
+const adjustScrollTracker = () => {
+    let totalDocHeight = Math.max(
+        body.scrollHeight, html.scrollHeight,
+        body.offsetHeight, html.offsetHeight,
+        body.clientHeight, html.clientHeight
+      );
+    let clientHeight = html.clientHeight;
+    heightToScroll = totalDocHeight - clientHeight;
+
+    window.addEventListener('scroll', () => {
+        let scrolledFromTop = html.scrollTop;
+        let heightLeftToScroll = heightToScroll - scrolledFromTop;
+        
+        let rawScrolledPercentage = 100 - ((heightLeftToScroll/heightToScroll)*100);
+        let ceiledScrollPercentage = Math.round(rawScrolledPercentage);
+      
+        scrollTracker.style.width=`${ceiledScrollPercentage}%`
+    });
+}
